b430539e41131843c88bb18c2eef1a460611e076
Misaf <misaf.1990@gmail.com>
2026-05-13 13:06:39 +0330

    feat: add German translations

---
+++ resources/lang/de/navigation.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'blog'                          => 'Blogs',
+    'blog_management'               => 'Blog-Verwaltung',
+    'blog_post'                     => 'Blogbeiträge',
+    'blog_post_category'            => 'Blogbeitragskategorien',
+    'blog_post_category_management' => 'Blogbeitragskategorie-Verwaltung',
+    'blog_post_management'          => 'Blogbeitragsverwaltung',
+];
