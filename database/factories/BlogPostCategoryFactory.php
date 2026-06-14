bfb517279b1f2419c7cbd707892f0dea62cccf05
Misaf <misaf.1990@gmail.com>
2026-06-14 13:10:56 +0330

    Use model attribute for blog post category factory

--- database/factories/BlogPostCategoryFactory.php
+++ database/factories/BlogPostCategoryFactory.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Misaf\VendraBlog\Database\Factories;
 
+use Illuminate\Database\Eloquent\Factories\Attributes\UseModel;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 use Misaf\VendraBlog\Models\BlogPostCategory;
@@ -12,10 +13,9 @@ use Misaf\VendraTenant\Models\Tenant;
 /**
  * @extends Factory<BlogPostCategory>
  */
+#[UseModel(BlogPostCategory::class)]
 final class BlogPostCategoryFactory extends Factory
 {
-    protected $model = BlogPostCategory::class;
-
     public function definition(): array
     {
         return [
