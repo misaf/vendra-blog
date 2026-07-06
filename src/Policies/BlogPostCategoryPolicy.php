21e2f77e5c1b4192d5b0ed5cd523640d00fb1c62
Misaf <misaf.1990@gmail.com>
2026-07-06 13:06:29 +0330

    Clean up module autoload and policies

--- src/Policies/BlogPostCategoryPolicy.php
+++ src/Policies/BlogPostCategoryPolicy.php
@@ -5,71 +5,71 @@ declare(strict_types=1);
 namespace Misaf\VendraBlog\Policies;
 
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Contracts\Auth\Access\Authorizable;
 use Misaf\VendraBlog\Enums\BlogPostCategoryPolicyEnum;
 use Misaf\VendraBlog\Models\BlogPostCategory;
-use Misaf\VendraUser\Models\User;
 
 final class BlogPostCategoryPolicy
 {
     use HandlesAuthorization;
 
-    public function create(User $user): bool
+    public function create(Authorizable $user): bool
     {
-        return $user->can(BlogPostCategoryPolicyEnum::CREATE);
+        return $user->can(BlogPostCategoryPolicyEnum::CREATE->value);
     }
 
-    public function delete(User $user, BlogPostCategory $blogPostCategory): bool
+    public function delete(Authorizable $user, BlogPostCategory $blogPostCategory): bool
     {
-        return $user->can(BlogPostCategoryPolicyEnum::DELETE);
+        return $user->can(BlogPostCategoryPolicyEnum::DELETE->value);
     }
 
-    public function deleteAny(User $user): bool
+    public function deleteAny(Authorizable $user): bool
     {
-        return $user->can(BlogPostCategoryPolicyEnum::DELETE_ANY);
+        return $user->can(BlogPostCategoryPolicyEnum::DELETE_ANY->value);
     }
 
-    public function forceDelete(User $user, BlogPostCategory $blogPostCategory): bool
+    public function forceDelete(Authorizable $user, BlogPostCategory $blogPostCategory): bool
     {
-        return $user->can(BlogPostCategoryPolicyEnum::FORCE_DELETE);
+        return $user->can(BlogPostCategoryPolicyEnum::FORCE_DELETE->value);
     }
 
-    public function forceDeleteAny(User $user): bool
+    public function forceDeleteAny(Authorizable $user): bool
     {
-        return $user->can(BlogPostCategoryPolicyEnum::FORCE_DELETE_ANY);
+        return $user->can(BlogPostCategoryPolicyEnum::FORCE_DELETE_ANY->value);
     }
 
-    public function reorder(User $user): bool
+    public function reorder(Authorizable $user): bool
     {
-        return $user->can(BlogPostCategoryPolicyEnum::REORDER);
+        return $user->can(BlogPostCategoryPolicyEnum::REORDER->value);
     }
 
-    public function replicate(User $user, BlogPostCategory $blogPostCategory): bool
+    public function replicate(Authorizable $user, BlogPostCategory $blogPostCategory): bool
     {
-        return $user->can(BlogPostCategoryPolicyEnum::REPLICATE);
+        return $user->can(BlogPostCategoryPolicyEnum::REPLICATE->value);
     }
 
-    public function restore(User $user, BlogPostCategory $blogPostCategory): bool
+    public function restore(Authorizable $user, BlogPostCategory $blogPostCategory): bool
     {
-        return $user->can(BlogPostCategoryPolicyEnum::RESTORE);
+        return $user->can(BlogPostCategoryPolicyEnum::RESTORE->value);
     }
 
-    public function restoreAny(User $user): bool
+    public function restoreAny(Authorizable $user): bool
     {
-        return $user->can(BlogPostCategoryPolicyEnum::RESTORE_ANY);
+        return $user->can(BlogPostCategoryPolicyEnum::RESTORE_ANY->value);
     }
 
-    public function update(User $user, BlogPostCategory $blogPostCategory): bool
+    public function update(Authorizable $user, BlogPostCategory $blogPostCategory): bool
     {
-        return $user->can(BlogPostCategoryPolicyEnum::UPDATE);
+        return $user->can(BlogPostCategoryPolicyEnum::UPDATE->value);
     }
 
-    public function view(User $user, BlogPostCategory $blogPostCategory): bool
+    public function view(Authorizable $user, BlogPostCategory $blogPostCategory): bool
     {
-        return $user->can(BlogPostCategoryPolicyEnum::VIEW);
+        return $user->can(BlogPostCategoryPolicyEnum::VIEW->value);
     }
 
-    public function viewAny(User $user): bool
+    public function viewAny(Authorizable $user): bool
     {
-        return $user->can(BlogPostCategoryPolicyEnum::VIEW_ANY);
+        return $user->can(BlogPostCategoryPolicyEnum::VIEW_ANY->value);
     }
 }
