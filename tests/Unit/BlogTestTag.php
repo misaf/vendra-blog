ba7c6f60549c89486ab62943c9de6b48313c232c
misaf <misaf.1990@gmail.com>
2026-07-14 12:47:52 +0000

    Fix test fixture autoloading

---
+++ tests/Unit/BlogTestTag.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Misaf\VendraBlog\Tests\Unit;
+
+use Illuminate\Database\Eloquent\Model;
+
+final class BlogTestTag extends Model
+{
+    protected $table = 'tags';
+}
--- tests/Unit/TagIntegrationTest.php
+++ tests/Unit/TagIntegrationTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Misaf\VendraBlog\Tests\Unit;
 
-use Illuminate\Database\Eloquent\Model;
 use Misaf\VendraBlog\Models\BlogPost;
 use Misaf\VendraSupport\Contracts\TagResolver;
 use Misaf\VendraSupport\Support\EloquentTagResolver;
@@ -25,8 +24,3 @@ it('builds a blog typed tag relation through the support contract', function ():
             'boolean'  => 'and',
         ]);
 });
-
-final class BlogTestTag extends Model
-{
-    protected $table = 'tags';
-}
